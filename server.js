98de01fdb23d15478a54634377e6f381c9aba4d7
unknown <g.vola.3347@vallauri.net>
2025-11-06 10:37:44 +0100

    add client server

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/home.html'); // Serve il file index.html
+});
+
+app.get('/random-number', (req, res) => {
+    
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    res.json({ number: randomNumber });
+});
+
+app.listen(port, () => {
+    console.log(`Server in ascolto su http://localhost:${port}`);
+});
